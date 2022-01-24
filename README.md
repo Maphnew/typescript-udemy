0145ebc96689dd9f1ec61ddbe742857d3a37b8fb
maphnew <zcm3118@gmail.com>
2022-01-24 22:28:18 +0900

    ### 84. More on Type Guards

--- README.md
+++ README.md
@@ -1665,7 +1665,74 @@ type Universal = Combinable & Numeric;
 
 11분
 
+```ts
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+```
+
+```ts
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log(emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log(emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "newMaph", startDate: new Date() });
+```
+
+```ts
+class Car {
+  drive() {
+    console.log("Drive...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Drive truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo ... " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+```
+
 ### 85. Discriminated Unions
+
+```
+tsc -w
+```
+
+```
+yarn start
+```
 
 6분
 
--- Section06/src/app.ts
+++ Section06/src/app.ts
@@ -1,22 +1,75 @@
 type Admin = {
-    name: string;
-    privileges: string[];
+  name: string;
+  privileges: string[];
 };
 
 type Employee = {
-    name: string;
-    startDate: Date;
+  name: string;
+  startDate: Date;
 };
 
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-    name: 'maph',
-    privileges: ['create-server'],
-    startDate: new Date()
-}
+  name: "maph",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
 
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log(emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log(emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "newMaph", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Drive...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Drive truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo ... " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
