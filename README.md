da2b2cd7311fb95d0c03ff61bc287e923ad80b50
maphnew <zcm3118@gmail.com>
2022-03-08 23:49:42 +0900

    ### 107. Building More Useful Decorators

--- README.md
+++ README.md
@@ -2255,6 +2255,40 @@ console.log(pers);
 
 6분
 
+```js
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Maph";
+
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
+```
+
 ### 108. Adding Multiple Decorators
 
 3분
--- Section08/src/app.ts
+++ Section08/src/app.ts
@@ -32,6 +32,28 @@
 
 // ### 106. Working with Decorator Factories
 
+// function Logger(logString: string) {
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// @Logger("LOGGING - PERSON")
+// class Person {
+//   name = "Maph";
+
+//   constructor() {
+//     console.log("Creating person object...");
+//   }
+// }
+
+// const pers = new Person();
+
+// console.log(pers);
+
+// ### 107. Building More Useful Decorators
+
 function Logger(logString: string) {
   return function (constructor: Function) {
     console.log(logString);
@@ -39,7 +61,19 @@ function Logger(logString: string) {
   };
 }
 
-@Logger("LOGGING - PERSON")
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Maph";
 
