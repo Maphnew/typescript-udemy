2f18273e1c7285530c661f86ed10dd1a70357cf1
maphnew <zcm3118@gmail.com>
2022-03-11 23:00:44 +0900

    ### 114. Example: Creating an "Autobind" Decorator

--- Section08/src/app.ts
+++ Section08/src/app.ts
@@ -42,6 +42,7 @@ class Person {
 // console.log(pers);
 
 // ====================================
+// ### 113. Other Decorator Return Types
 
 function Log(target: any, propertyName: string | Symbol) {
   console.log("Property decorator!");
@@ -97,3 +98,31 @@ class Product {
 
 const p1 = new Product("Book", 19);
 const p2 = new Product("Book2", 29);
+
+// ### 114. Example: Creating an "Autobind" Decorator
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+const p = new Printer();
+p.showMessage();
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
